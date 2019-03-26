4f6670bb693d31e1841a843153850c46bbcf6c5d
davidhonasan <david_honasan@live.com>
2019-03-26 16:36:13 +0700

    sass to scss

--- src/app/components/planner-input/planner-input.component.ts
+++ src/app/components/planner-input/planner-input.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-planner-input',
   templateUrl: './planner-input.component.html',
-  styleUrls: ['./planner-input.component.sass']
+  styleUrls: ['./planner-input.component.scss']
 })
 export class PlannerInputComponent implements OnInit {
 
